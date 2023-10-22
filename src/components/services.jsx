3265f7baa3f75c2a7ea9d9f9bb2ae8e4930e5b51
Daniel M Piccoli <daniel.piccoli@datacake.com.br>
2023-10-22 10:43:50 -0300

    O que proteger precisa de ajustes de css

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -3,17 +3,16 @@ import React from "react";
 export const Services = (props) => {
   return (
     <div id="services" className="text-center">
-      <div className="container">
+      <div id={props?.data?.id ? props.data.id : "services"} className="container">
         <div className="section-title">
-          <h2>Our Services</h2>
+          <h2>{props?.data?.title ? props.data.title : "...loading"}</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            { props?.data?.info ? props.data.info : "...loading"}
           </p>
         </div>
         <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
+          {props?.data?.items
+            ? props.data.items.map((d, i) => (
                 <div key={`${d.name}-${i}`} className="col-md-4">
                   {" "}
                   <i className={d.icon}></i>
@@ -25,6 +24,12 @@ export const Services = (props) => {
               ))
             : "loading"}
         </div>
+        <div>
+          <p>
+            { props?.data?.postInfo ? props.data.postInfo : "...loading"}
+          </p>
+        </div>
+        
       </div>
     </div>
   );
