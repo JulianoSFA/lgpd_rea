3ec330cfac84dbca6b022bacf980ac6649509ebb
Juliano Sartorelli <juliano.franco@datacake.com.br>
2023-11-07 21:28:05 -0300

    Doing the footer

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -149,10 +149,10 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
-            </a>
+            &copy; 2023 Recurso Educacional Aberto (REA) desenvolvido pelos alunos da
+            <a href="https://univesp.br/" target="_blank">Univesp</a>,
+            disponível sob licença <a href={process.env.PUBLIC_URL + '/LICENSE.md'} target="_blank">MIT</a>
+
           </p>
         </div>
       </div>
