9820b5f7b6088a38d03c6012c631d82fe914e7b7
Daniel M Piccoli <daniel.piccoli@datacake.com.br>
2023-11-07 21:26:00 -0300

    Referências hardcoded

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Features } from "./components/features";
 import { About } from "./components/about";
 import { Services } from "./components/services";
 import { Contact } from "./components/contact";
+import { References } from "./components/references";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
@@ -27,6 +28,7 @@ const App = () => {
       <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
       <Services data={landingPageData.Recommendations} />
+      <References></References>
       <Contact data={landingPageData.Contact} />
     </div>
   );
