74c88370d74a84409499e39700e5a5cc665919ad
Juliano Sartorelli <juliano.franco@datacake.com.br>
2023-11-09 20:45:37 -0300

    Adding names and contacts

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,9 +8,11 @@ export const Footer = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Recurso Educacional Aberto (REA) desenvolvido pelos alunos da 
-            <a href="https://univesp.br/" target="_blank">Univesp</a>,
+            &copy; 2023 Recurso Educacional Aberto (REA) desenvolvido pelos alunos Juliano Sartorelli e Daniel Piccoli<br/>
+            Faculdade <a href="https://univesp.br/" target="_blank">Univesp</a>,
             disponível sob licença <a href={process.env.PUBLIC_URL + '/LICENSE.md'} target="_blank">MIT</a>
+            <br/>
+            Contatos no Whatsapp: (14) 99619-2682 / (14) 98102-7563
           </p>
         </div>
       </div>
